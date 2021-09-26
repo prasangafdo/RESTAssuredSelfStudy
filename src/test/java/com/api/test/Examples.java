cfc6ad527fa7a813ead8b931f34f559498b28f74
Prasanga Fernando <prasangafdz@outlook.com>
2021-09-26 11:39:15 +0530

    Started working on Rest Assured static imports

--- src/test/java/com/api/test/Examples.java
+++ src/test/java/com/api/test/Examples.java
@@ -1,18 +1,28 @@
 package com.api.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 public class Examples {
 	
 	@Test
 	public void sampleGet() {
-		
 		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
-		
 		System.out.println(response.asPrettyString());
+		Assert.assertEquals(response.getStatusCode(), 200);
 	}
+	@Test
+	public void sampleGet2() {
+		System.out.println(get("https://reqres.in/api/users?page=2"));
+	}
+	
 
 }
+	
