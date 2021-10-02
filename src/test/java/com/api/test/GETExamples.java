df53a4b6eae2f2b51140e64d36ea78f734be7edc
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-02 18:58:12 +0530

    Minor changes to method names

--- src/test/java/com/api/test/GETExamples.java
+++ src/test/java/com/api/test/GETExamples.java
@@ -10,33 +10,40 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
-public class Examples {
+public class GETExamples {
 	
 	@Test
 	public void sampleGet() {
 		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
 		System.out.println(response.asPrettyString());
 		Assert.assertEquals(response.getStatusCode(), 200);
+		System.out.println("Header >>>>>"+response.getHeader("content-type"));
 	}
 	
 	@Test
-	public void sampleGet2() {
+	public void sampleGetInGherkins() {
 		baseURI = "https://reqres.in/api";
 		given().
 			get("/users?page=2").
 		then().
 			statusCode(200);
+		System.out.println(">>>>> This will get the response using BDD style");
 		System.out.println(get("/users?page=3").getBody().asString());
 	}
 	
 	@Test
-	public void sampleGet3() {
+	public void sampleGetWithRestAssuredAssertion() {
 		baseURI = "https://reqres.in/api";
 		given().
 			get("/users?page=2").
 		then().
-			body("data[0].first_name", equalTo("Michael"));
+			body("data[0].first_name", equalTo("Michael")).
+			log().all();//This will print all the data on the response
+			
+		
 	}
+	
+
 
 }
 	
