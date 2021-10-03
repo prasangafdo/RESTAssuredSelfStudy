64b3b2691bc0a997906399c36b63499e565e696b
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-03 13:06:14 +0530

    Completed "SamplePostUsingHashMap" method

--- src/test/java/com/api/test/POSTExamples.java
+++ src/test/java/com/api/test/POSTExamples.java
@@ -7,6 +7,7 @@ import static org.hamcrest.Matchers.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 public class POSTExamples {
@@ -16,7 +17,24 @@ public class POSTExamples {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("name", "Prasanga");
 		map.put("job", "Quality Engineer");
+		
 		System.out.println(map);
+		
+		JSONObject request = new JSONObject(map);
+		System.out.println("JSON formatted object");
+		System.out.println(request);
+		System.out.println("Verifying the JSON formatted Object");
+		System.out.println(request.toJSONString());
+		
+		
+		System.out.println(">>> API Response");
+		baseURI = "https://reqres.in/api";
+		given().
+			body(request.toJSONString()).
+		when().
+			post("/users").
+		then().
+			statusCode(201).log().all();
 	}
 	
 }
