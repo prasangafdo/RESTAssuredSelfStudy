84c2337cda2c970a278fd921e5cdb924683741bf
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-03 10:13:59 +0530

    Started working on POST requests

---
+++ src/test/java/com/api/test/POSTExamples.java
@@ -0,0 +1,9 @@
+package com.api.test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class POSTExamples {
+
+}
