7ad2cb13021276a04a447d78014a8f7273facab0
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-03 11:23:04 +0530

    Created a hashmap to POST data in JSON format
    HashMap stores values in Key Value format that we can use to store JSON data

--- src/test/java/com/api/test/POSTExamples.java
+++ src/test/java/com/api/test/POSTExamples.java
@@ -4,6 +4,19 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
 public class POSTExamples {
 
+	@Test
+	public void SamplePostUsingHashMap() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "Prasanga");
+		map.put("job", "Quality Engineer");
+		System.out.println(map);
+	}
+	
 }
