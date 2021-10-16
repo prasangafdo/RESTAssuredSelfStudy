08d37ee421a9821256b0256a16b697d704870257
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-16 19:44:41 +0530

    Created a sample GET request for the fake API host

--- src/test/java/com/api/demo/GETExamples.java
+++ src/test/java/com/api/demo/GETExamples.java
@@ -4,11 +4,18 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.annotations.Test;
+
 public class GETExamples {
 	/*
 	 * I've hosted a fake JSON server in localhost and pointing my get request to that server in this class
 	 */
-	
+	@Test
+	public void getAllPosts() {
+		baseURI = "http://localhost:3000/";
+		
+		given().get("/posts").then().body("[0].title", equalTo("json-server")).statusCode(200).log().all();
+	}
 	
 
 }
