a2faf969864d87556633c6e9c670e8e56ae4363e
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-16 19:31:01 +0530

    Created a new class to work with GET requests
    Pointed to localhost fake JSON server

---
+++ src/test/java/com/api/demo/GETExamples.java
@@ -0,0 +1,14 @@
+package com.api.demo;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class GETExamples {
+	/*
+	 * I've hosted a fake JSON server in localhost and pointing my get request to that server in this class
+	 */
+	
+	
+
+}
