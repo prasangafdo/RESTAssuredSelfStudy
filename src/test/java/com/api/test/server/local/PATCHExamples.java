ba5c7d66d572c75b3f83bc788525ff5755799e27
Prasanga Fernando <prasangafdz@outlook.com>
2021-10-19 19:33:55 +0530

    Added a patch request to update the post title

--- src/test/java/com/api/test/server/local/PATCHExamples.java
+++ src/test/java/com/api/test/server/local/PATCHExamples.java
@@ -4,15 +4,33 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
 
 
 public class PATCHExamples {
 	
+	@SuppressWarnings("unchecked")
 	@Test
-	public void UpdatePost() {
+	public void UpdatePostTitle() {
 	baseURI = "http://localhost:3000/";
 	
+	//Change the post ID and value here
+	int postID = 5;
+	String value = "This is the patch request updated value";
+	
+	
+	JSONObject jsonObject = new JSONObject();
+	jsonObject.put("title", value);
+	
+	given().
+		contentType(ContentType.JSON).accept(ContentType.JSON).body(jsonObject.toJSONString()).
+	when().
+		patch("posts/"+postID).
+	then().
+		statusCode(200).log().body();
 	
 	}
 	
